37d160d128eec6fe5f176a6b0f7fd403e38ee7f5
GrooviePie <137789362+GrooviePie@users.noreply.github.com>
2023-10-04 10:51:46 +0200

    Add files via upload

---
+++ GigaStats/app/src/main/java/com/example/giga_stats/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.giga_stats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
