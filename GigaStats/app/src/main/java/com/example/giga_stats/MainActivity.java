378b59db921efcb6d3a62b1fee192dc28c2efcb5
argi0001 <argi0001@stud.hs-kl.de>
2023-10-05 20:53:34 +0200

    Fragments + Lifecycle
    Fragmente eingebunden mit dem BottomNavMenu + Lebenszyklusmethoden-Gerüst hinzugefügt

--- GigaStats/app/src/main/java/com/example/giga_stats/MainActivity.java
+++ GigaStats/app/src/main/java/com/example/giga_stats/MainActivity.java
@@ -1,15 +1,118 @@
 package com.example.giga_stats;
 
+
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        // Erstelle eine Instanz des FragmentManagers
+        fragmentManager = getSupportFragmentManager();
+
+        // Zeige standardmäßig das StatisticsFragment an
+         showStatisticsFragment();
+
+        // Konfiguriere die Bottom Navigation View
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        // Verwende setOnItemSelectedListener anstelle von setOnNavigationItemSelectedListener
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.STATS_BUTTONMENU_BUTTON) {
+                showStatisticsFragment();
+                return true;
+            } else if (itemId == R.id.TIMER_BUTTONMENU_BUTTON) {
+                showTimerFragment();
+                return true;
+            } else if (itemId == R.id.EXERCISES_BUTTONMENU_BUTTON) {
+                showExercisesFragment();
+                return true;
+            } else if (itemId == R.id.WORKOUT_BUTTONMENU_BUTTON) {
+                showWorkoutsFragment();
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void showFragment(Fragment fragment) {
+        // Beginne eine Transaktion, um das Fragment hinzuzufügen
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.FrameLayout, fragment); // "FrameLayout" ist die ID des Containers, in dem das Fragment angezeigt werden soll
+        transaction.addToBackStack(null); // Optional: Füge die Transaktion zum Back-Stack hinzu, um den Zurück-Knopf zu unterstützen
+        transaction.commit();
+    }
+
+    private void showStatisticsFragment() {
+        StatisticsFragment statisticsFragment = new StatisticsFragment();
+        showFragment(statisticsFragment);
+    }
+
+    private void showTimerFragment() {
+        TimerFragment timerFragment = new TimerFragment();
+        showFragment(timerFragment);
+    }
+
+    private void showExercisesFragment() {
+        ExercisesFragment exercisesFragment = new ExercisesFragment();
+        showFragment(exercisesFragment);
+    }
+
+    private void showWorkoutsFragment() {
+        WorkoutsFragment workoutsFragment = new WorkoutsFragment();
+        showFragment(workoutsFragment);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("CHAD", "onStart(): Die Activity wird bald komplett sichtbar");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("CHAD", "onResume(): Die Activity ist komplett sichtbar und aktiv");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("CHAD", "onPause(): Eine andere Activity ist jetzt im Fokus");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("CHAD", "onStop(): Activity ist vollständig nicht mehr sichtbar");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("CHAD", "onRestart(): Die Activity wird erneut gestartet");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("CHAD", "onDestroy(): Letzter Aufruf der Activity vor der Zerstörung ");
     }
 }
