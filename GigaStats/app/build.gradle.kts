94d426c03525044be5980f659d89dec9bef75a45
argi0001 <argi0001@stud.hs-kl.de>
2024-01-09 17:31:15 +0100

    dependencies sdk 33 Path auskommentiert in build.gradle.kts

--- GigaStats/app/build.gradle.kts
+++ GigaStats/app/build.gradle.kts
@@ -40,23 +40,21 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("androidx.room:room-runtime:2.4.0")
-    implementation("androidx.room:room-common:2.5.2")
-    implementation(fileTree(mapOf(
-        "dir" to "C:\\Users\\giber\\AppData\\Local\\Android\\Sdk\\platforms\\android-33",
-        "include" to listOf("*.aar", "*.jar")
-
-    )))
-    annotationProcessor ("androidx.room:room-compiler:2.4.0")
+    implementation ("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-common:2.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("androidx.viewpager2:viewpager2:1.0.0")
-    implementation ("com.google.android.material:material:1.4.0") // Für die TabLayout-Integration
 
-
+    /*
+    implementation(fileTree(mapOf(
+        "dir" to "C:\\Users\\giber\\AppData\\Local\\Android\\Sdk\\platforms\\android-33",
+        "include" to listOf("*.aar", "*.jar")
+    )))
+    */
 }
 
 
